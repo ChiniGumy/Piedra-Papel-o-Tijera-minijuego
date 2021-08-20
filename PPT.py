510490aee9a85e6bad6eedf875a98a6f98f4d7d9
ChiniGumy <dsebastiansr@hotmail.com>
2021-08-19 21:48:58 -0500

    v2.0

--- PPT.py
+++ PPT.py
@@ -1,42 +1,85 @@
 import random
 
-print('\n')
+def validacion_ingreso():
+    validacion = True
+
+    while validacion:
+
+        opcion_humano = input('ingresa tu opcion.. ')
+        if opcion_humano.isdigit():
+            opcion_humano = int(opcion_humano)
+            if  0 <= opcion_humano <= 2:
+                validacion = False
+            else:
+                print('el numero ingresado debe estar entre 0 y 2')
+        else:
+            print('caracter invalido, porfavor ingresar de nuevo\n')
+
+    return opcion_humano
+
 print('bienvenido a Piedra, Papel o Tijera 2021')
-print('para usar piedra introduce 0')
-print('para usar papel  introduce 1')
-print('para usar tijera introduce 2\n')
 
-opcion_humano = int(input('ingresa tu opcion.. '))
-if opcion_humano == 0:
-  respuesta_humano = 'piedra'
-elif opcion_humano == 1:
-  respuesta_humano = 'papel'
-elif opcion_humano == 2:
-  respuesta_humano = 'tijera'
+seguir_jugando = True
+        
+while seguir_jugando:
+    
+    print('para usar piedra introduce 0')
+    print('para usar papel  introduce 1')
+    print('para usar tijera introduce 2\n')
 
-opcion_pc = random.randint(0,2)
-if opcion_pc == 0:
-    respuesta_pc = 'piedra'
-elif opcion_pc == 1:
-    respuesta_pc = 'papel'
-elif opcion_pc == 2:
-    respuesta_pc = 'tijera'
+    opcion_humano = validacion_ingreso()
 
-#print('\n')
-print(f'seleccionaste: {respuesta_humano}')
-print(f'seleccion de la pc: {respuesta_pc}\n')
+    if opcion_humano == 0:
+        respuesta_humano = 'piedra'
+    elif opcion_humano == 1:
+        respuesta_humano = 'papel'
+    elif opcion_humano == 2:
+        respuesta_humano = 'tijera'
 
-if respuesta_humano == 'piedra' and respuesta_pc == 'tijera':
-    print('ganaste!')
-elif respuesta_humano == 'papel' and respuesta_pc == 'piedra':
-    print('ganaste!')
-elif respuesta_humano == 'tijera' and respuesta_pc == 'papel':
-    print('ganaste!')
-elif respuesta_pc == 'piedra' and respuesta_humano == 'tijera':
-    print('perdiste!')
-elif respuesta_pc == 'papel' and respuesta_humano == 'piedra':
-    print('perdiste!')
-elif respuesta_pc == 'tijera' and respuesta_humano == 'papel':
-    print('perdiste!')
-else:
-    print('empate ._.')
+    opcion_pc = random.randint(0,2)
+    if opcion_pc == 0:
+        respuesta_pc = 'piedra'
+    elif opcion_pc == 1:
+        respuesta_pc = 'papel'
+    elif opcion_pc == 2:
+        respuesta_pc = 'tijera'
+
+    print(f'seleccionaste: {respuesta_humano}')
+    print(f'seleccion de la pc: {respuesta_pc}')
+
+    if respuesta_humano == 'piedra' and respuesta_pc == 'tijera':
+        print('ganaste!')
+    elif respuesta_humano == 'papel' and respuesta_pc == 'piedra':
+        print('ganaste!')
+    elif respuesta_humano == 'tijera' and respuesta_pc == 'papel':
+        print('ganaste!')
+    elif respuesta_pc == 'piedra' and respuesta_humano == 'tijera':
+        print('perdiste!')
+    elif respuesta_pc == 'papel' and respuesta_humano == 'piedra':
+        print('perdiste!')
+    elif respuesta_pc == 'tijera' and respuesta_humano == 'papel':
+        print('perdiste!')
+    else:
+        print('empate ._.')
+
+    print('')
+    jugar_denuevo_pregunta = input('desea jugar de nuevo? y/n.. ')
+    print('')
+
+
+    validacion_volverjugar = True
+
+    while validacion_volverjugar:
+
+        if jugar_denuevo_pregunta == 'y':
+            validacion_volverjugar = False
+            seguir_jugando = True
+
+        elif jugar_denuevo_pregunta == 'n':
+            validacion_volverjugar = False
+            seguir_jugando = False
+
+        else: 
+            print('ingresar y/n para definir la accion')
+            jugar_denuevo_pregunta = input('desea jugar de nuevo? y/n..')
+            print('')
---
+++ README.md
@@ -0,0 +1,14 @@
+# v2.0 Notas de Parche
+
+**\+** El juego pregunta si quieres volver a jugar.
++ Esto permite al usuario poner un "y" para aceptar y volver a jugar y un "n" para no volver a jugar y cerrar el juego.
+
+**\+** Validaciones de ingresos
+
++ En vista de que habian graciositos que cuando pedian el ingreso de la opcion, ingresaban caracteres o numeros que se sobrepasaban
+    implementamos las validaciones de ingreso en:
+
+        ingresar opcion (piedra, papel o tijera)
+        ingresar y/n (para seguir el programa o no)
+
+**\-** Restamos bugs
